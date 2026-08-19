99847de5a600bc63a040504bcf02944d42d8fd5b
𝔏𝔢 ℭ𝔞𝔳𝔞𝔩𝔦𝔢𝔯 ℜ𝔦𝔞𝔫𝔱 <lecavalierriant@gmail.com>
2026-08-19 22:10:00 +0200

    Update polygramme.ps1

--- polygramme.ps1
+++ polygramme.ps1
@@ -3,28 +3,29 @@
 	@{original = "speciaux/apos"; copies = @("speciaux/DiacriticalAcute")},
 	@{original = "speciaux/DiacriticalTilde"; copies = @("speciaux/0x1FC0")},
 	
-	# lt copies = @("gr", "ru", "00")
+	# lt copies = @("")
 	@{original = "A"; copies = @("Α", "А")},
 		@{original = "Á"; copies = @("Ά", "Ά")},
 		@{original = "À"; copies = @("Ὰ")},
-		@{original = "Ä"; copies = @(<#"", #>"Ӓ")},
+		@{original = "Ä"; copies = @("Ӓ")},
 		@{original = "Ă"; copies = @("Ᾰ", "Ӑ")},
 		@{original = "Ā"; copies = @("Ᾱ")},
-	@{original = "Æ"; copies = @(<#"", #>"Ӕ")},
+	@{original = "Æ"; copies = @("Ӕ")},
 	@{original = "B"; copies = @("Β", "В")},
 	@{original = "C"; copies = @("Ϲ", "С", "speciaux/lpar")},
+		@{original = "Đ"; copies = @("Ð")},
 	@{original = "E"; copies = @("Ε", "Е")},
 		@{original = "É"; copies = @("Έ","Έ")},
 		@{original = "È"; copies = @("Ὲ", "Ѐ")},
-		@{original = "Ë"; copies = @(<#"", #>"Ё")},
-		@{original = "Ĕ"; copies = @(<#"", #>"Ӗ")},
+		@{original = "Ë"; copies = @("Ё")},
+		@{original = "Ĕ"; copies = @("Ӗ")},
 	@{original = "F"; copies = @("Ϝ")},
 	@{original = "H"; copies = @("Η", "Н")},
 	@{original = "I"; copies = @("Ι", "І", "1")},
 		@{original = "Í"; copies = @("Ί")},
 		@{original = "Ï"; copies = @("Ϊ", "Ї")},
 	@{original = "K"; copies = @("Κ", "К")},
-		@{original = "Ḱ"; copies = @(<#"", #>"Ќ")},
+		@{original = "Ḱ"; copies = @("Ќ")},
 	@{original = "M"; copies = @("Μ", "М")},
 	@{original = "N"; copies = @("Ν")},
 	@{original = "O"; copies = @("Ο", "О", "0")},
@@ -37,10 +38,10 @@
 	@{original = "Y"; copies = @("Υ", "У")},
 		@{original = "Ý"; copies = @("Ύ")},
 		@{original = "Ÿ"; copies = @("Ϋ", "Ӱ")},
-		@{original = "Ȳ"; copies = @(<#"", #>"Ӯ")},
+		@{original = "Ȳ"; copies = @("Ӯ")},
 	@{original = "Z"; copies = @("Ζ")},
 
-	# gr copies = @("ru", "00")
+	# gr copies = @("")
 	@{original = "Γ"; copies = @("Г")},
 	@{original = "Δ"; copies = @("Д")},
 	@{original = "Θ"; copies = @("Ѳ")},
@@ -48,7 +49,7 @@
 	@{original = "Π"; copies = @("П")},
 	@{original = "Φ"; copies = @("Ф")},
 
-	# ru copies = @("00")
+	# ru copies = @("")
 	@{original = "З"; copies = @("3")},
 	@{original = "Ч"; copies = @("4")}
 
